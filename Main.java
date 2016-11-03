76068b7af6eb815c310024b1bd00b6888438c1f1
28kayak <kaya.ota@sjsu.edu>
2016-11-03 15:25:14 -0700

    update

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 /**
  * Created by kaya on 10/24/16.
  */
+package
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -19,6 +21,8 @@ public class Main
         String inputFileName = "/Users/kaya/IdeaProjects/JavaSampleCompiler/src/test1.txt";
         File inputFile = new File(inputFileName);
         String[] statement;
+        String[] imported;
+        int numOfImport = 0;
 
         try
         {
@@ -35,23 +39,38 @@ public class Main
             System.out.println("line = " + line);
 
             while (!line.contains("class"))
-            {//lines are package declaration or import declaration
+            {//lines contained package declaration or import declaration
                 //before class declaration
                 statement = line.split(" ");
                 System.out.println("statement[0] = " + statement[0]);
                 if(statement[0].equals("import"))
                 {
+
                     //check if package or import statement is correct
                     //importCheckr(statement[1], lineNum);
+                    imported[numOfImport] =  " ";
+                    numOfImport++;
 
                     System.out.println("statement[1] = " + statement[1]);
                 }
                 else if (statement[0].equals("package"))
                 {
-                    packageChecker(statement[1],lineNum);
+                    flag = 0;
+                    if (flag <= 2)
+                    {
+                        System.out.println("ERROR: multiple package statements");
+                    }
+                    else
+                    {
+                         packageChecker(statement[1],lineNum);
+                        flag++;
+                    }
+
+
 
                 }else if(statement[0].equalsIgnoreCase("import") || statement[0].equalsIgnoreCase("package"))
                 {
+                    //import or package is coming using big case
                     System.out.println("ERROR: java is case sensitive: @ "+ lineNum );
                 }
                 else
@@ -67,8 +86,10 @@ public class Main
             while(line!=null)
             {
                 //class declaration starts
-                //for now infinite loop
+
                 //System.out.println("class");
+                line = br.readLine();
+                lineNum++;
             }
             System.out.println("the last line");
             System.out.println(lineNum);
@@ -96,7 +117,7 @@ public class Main
     {
         boolean validity = false;
         System.out.println("candidate = "+candidate);
-        //String[] splited = candidate.split(".");
+        //String[] splited = candidate.split("\\.");
         //System.out.println("spliet = " + splited[0]);
         if(candidate.endsWith(";"))
         {
@@ -119,6 +140,7 @@ public class Main
     }//packageChecker
     static  boolean importChecker(String candidate, int lineNum)
     {
+
         boolean validity = false;
 
 
@@ -126,5 +148,17 @@ public class Main
 
         return validity;
 
+    }
+    static boolean isImported(String [] imported, int numOfimported, String newImport)
+    {
+        for (int i = 0; i <= imported.length; i++)
+        {
+            if(imported[i].equalsIgnoreCase(newImport))
+            {
+                return false;
+            }
+
+        }
+        return true;
     }
 }//class
